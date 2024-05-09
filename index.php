8300810ac9d3480a4e91b65a72f5ed9f3f90d89a
BobLiu <bobliu@bobliu.tech>
2024-05-09 10:24:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <?php
 
+function completeRouteName($routeName){
+    return $routeName.(preg_match('/^[A-Z]+[0-9]*$/', $routeName) ? '路' : '');
+}
+
 header('Content-Type: application/csv');
 $data = json_decode(file_get_contents('vehicles.json'), true);
 header('Content-Disposition: attachment; filename=长春公交 '.$data['time'].'.csv');
-$csv = '车牌号,线路,自编号';
+$csv = '车牌号,当前线路,自编号,最后上线时间,线路';
 foreach($data['vehicleList'] as $vehicle){
-    if(preg_match('/^(高新一实验|东安实验|八十七中|103中学|附中新城)[0-9]号线$/', $vehicle['lastOnLine'])){
-        continue;
-    }else if(preg_match('/^[A-Z]+[0-9]*$/', $vehicle['lastOnLine'])){
-        $vehicle['lastOnLine'] .= '路';
-    }
+    // if(preg_match('/^(高新一实验|东安实验|八十七中|103中学|附中新城)[0-9]号线$/', $vehicle['lastOnLine'])){
+    // 	continue;
+    // }
 
-    if(!is_numeric($vehicle['busId'])){
+    if(!preg_match('/^[0-9]+$/', $vehicle['busId'])){
         $vehicle['busId'] = '';
     }
 
-    $csv .= "\n".implode(',', [$vehicle['busLicense'], $vehicle['lastOnLine'], $vehicle['busId']]);
+    $allLines = [];
+    foreach($vehicle['onLine'] as $line){
+        $allLines[] = completeRouteName($line['line']);
+    }
+
+    $csv .= "\n".implode(',', [$vehicle['busLicense'], completeRouteName($vehicle['lastOnLine']), $vehicle['busId'],
+        $vehicle['onLine'][count($vehicle['onLine']) - 1]['lastFound'], implode('/', array_unique($allLines))]);
 }
 echo iconv('UTF-8', 'GBK', $csv);
