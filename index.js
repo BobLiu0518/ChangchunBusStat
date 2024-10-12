a6be90650beabca99f3314ae7058186efa8cbe32
BobLiu <bobliu@bobliu.tech>
2024-10-12 23:14:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ import moment from 'moment';
 import CCBUS from './ccbus.js';
 
 let ccbus = new CCBUS();
-let lineList = ccbus.getLineList();
+let lineList;
 let vehicleList;
 let fileName = 'vehicles.json';
 vehicleList = fs.readFileSync(fileName, 'UTF-8') ?? '{}';
 vehicleList = JSON.parse(vehicleList)['vehicleList'];
+
+function updateLineList() {
+    lineList = ccbus.getLineList();
+}
 
 function update() {
     let time = moment().format('YYYY/MM/DD HH:mm:ss');
@@ -30,7 +34,49 @@ function update() {
                             vehicleList[vehicle.busNoChar].onLine.push({
                                 line: line.lineName,
                                 time,
+                                lastFound: time,
                             });
+                        } else {
+                            vehicleList[vehicle.busNoChar].onLine[
+                                vehicleList[vehicle.busNoChar].onLine.length - 1
+                            ].lastFound = time;
+                        }
+
+                        if (
+                            vehicle.busNo !=
+                            vehicleList[vehicle.busNoChar].busId
+                        ) {
+                            console.log(
+                                'Info',
+                                time,
+                                `${vehicle.busNoChar} changed id ${
+                                    vehicleList[vehicle.busNoChar].busId
+                                }=>${vehicle.busNo}`
+                            );
+                            vehicleList[vehicle.busNoChar].busId =
+                                vehicle.busNoChar;
+                        }
+                        
+                        if (
+                            !vehicleList[
+                                vehicle.busNoChar
+                            ].onlineDate.hasOwnProperty(
+                                moment().format('YYYY/MM/DD')
+                            )
+                        ) {
+                            vehicleList[vehicle.busNoChar].onlineDate[
+                                moment().format('YYYY/MM/DD')
+                            ] = [line.lineName];
+                        } else {
+                            if (
+                                !vehicleList[vehicle.busNoChar].onlineDate[
+                                    moment().format('YYYY/MM/DD')
+                                ].includes(line.lineName)
+                            ) {
+                                vehicleList[vehicle.busNoChar].onlineDate[
+                                    moment().format('YYYY/MM/DD')
+                                ].push(line.lineName);
+                            }
                         }
                     } else {
                         console.log(
@@ -48,9 +94,13 @@ function update() {
                                 {
                                     line: line.lineName,
                                     time,
+                                    lastFound: time,
                                 },
                             ],
                             lastOnLine: line.lineName,
+                            onlineDate: {
+                                [moment().format('YYYY/MM/DD')]: line.lineName,
+                            },
                         };
                     }
                 });
@@ -66,9 +116,12 @@ function update() {
             }
         );
     } catch (e) {
+        updateLineList();
         console.error('Error', time, e);
     }
 }
 
+updateLineList();
 update();
 setInterval(update, 15 * 60 * 1000);
+setInterval(updateLineList, 6 * 60 * 60 * 1000);
